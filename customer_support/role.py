bd4bae011bc3a19af4e64bf7a1171afdcfce1965
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-07-07 20:28:55 +0800

    testing firebase

--- customer_support/role.py
+++ customer_support/role.py
@@ -1,14 +1,19 @@
 from ticket import Ticket
+from db_setup import database_init
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import credentials
+import json
 
-cred = credentials.Certificate("/Users/daaa/Downloads/KAKI-App/customer_support/serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://kaki-db097-default-rtdb.asia-southeast1.firebasedatabase.app/'
+database_init()
+
+# print(json.dumps(db.reference("Users").get(), indent=4))
+kakiDB = db.reference("user")
+
+kakiDB.set({
+    "email": "johnwick@gmail.com",
+    "name": "John Wick"
 })
-ref = db.reference('Users')
-
 
 class User: 
     def __init__(self, role_id, email): 
@@ -20,6 +25,9 @@ class User:
         self.ticket = Ticket(1, 1, 1, subject)
         self.ticket.addDescription(description)
         self.ticket.addTopic("technical support")
+        kakiDB.child("email").set({
+            "ticket": json.load(self.ticket.__str__())
+        })
 
     def update_ticket(self):
         pass
@@ -38,8 +46,6 @@ class Staff:
     pass
 
 if __name__ == "__main__":
-    user = User(1, "karthik@gmail.com")
-    user.create_ticket("I am not able to login", "I am not able to login to my account, please help me")
-    print(user.view_ticket())
-    user.delete_ticket()
+    user = User(1, "kars@gmail.com")
+    user.create_ticket("app is broken", "this app sux")
     print(user.view_ticket())
