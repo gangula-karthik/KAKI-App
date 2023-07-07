eccf837fa3c163ae92a7e116f704329e1af2b681
nightsinker <nightsinker.2005@gmail.com>
2023-07-08 00:15:49 +0800

    js n css in static and sign up page can be seen

--- Login/authentication.py
+++ Login/authentication.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
@@ -8,23 +8,27 @@ app = Flask(__name__)
 cred = credentials.Certificate("Login/credentials.json")
 firebase_admin.initialize_app(cred)
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('Login/login.html')
+    return render_template('login.html')
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    email = request.form.get('email')
-    password = request.form.get('password')
+@app.route('/logout')
+def logout():
+    pass
 
-    try:
-        user = auth.create_user(
-            email=email,
-            password=password
-        )
-        return "User created successfully: " + user.uid
-    except Exception as e:
-        return "Error creating user: " + str(e)
+# @app.route('/signup', methods=['POST'])
+# def signup():
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+
+#     try:
+#         user = auth.create_user(
+#             email=email,
+#             password=password
+#         )
+#         return "User created successfully: " + user.uid
+#     except Exception as e:
+#         return "Error creating user: " + str(e)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=1111)
