d8647690369e26e05fecd9d53a195d7540c64770
nightsinker <nightsinker.2005@gmail.com>
2023-07-09 22:01:37 +0800

    O shit i forgot to push

--- authentication.py
+++ authentication.py
@@ -5,33 +5,33 @@ from firebase_admin import auth
 
 app = Flask(__name__)
 
-cred = credentials.Certificate("Account_management/credentials.json")
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate("Account_management/credentials.json")
+# firebase_admin.initialize_app(cred)
 
 @app.route('/')
 def index():
     return render_template('account_management/login.html')
 
 
-# @app.route('/create_account',methods=['GET','POST'])
-# def create_account():
-#     if request.method == 'POST':
-#         pwd0 = request.form['user_pwd0']
-#         pwd1 = request.form['user_pwd1']
-#         if pwd0 == pwd1:
-#             email = request.form['user_email']
-#             try:
-#                 user = auth.create_user(
-#                     email=email,
-#                     password=pwd0
-#                 )
-#                 auth.send_email_verification(user.email)
-#                 auth.generate_email_verification_link(user['idToken'])
-#                 return render_template('account_management/verify_email.html')
-#             except:
-#                 existing_account = "An account with this email already exists."
-#                 return render_template('account_management/login.html', exist_message=existing_account)
-#     return render_template('account_management/login.html')
+@app.route('/create_account',methods=['GET','POST'])
+def create_account():
+    if request.method == 'POST':
+        pwd0 = request.form['user_pwd0']
+        pwd1 = request.form['user_pwd1']
+        if pwd0 == pwd1:
+            email = request.form['user_email']
+            try:
+                user = auth.create_user(
+                    email=email,
+                    password=pwd0
+                )
+                auth.send_email_verification(user.email)
+                auth.generate_email_verification_link(user['idToken'])
+                return render_template('account_management/verify_email.html')
+            except:
+                existing_account = "An account with this email already exists."
+                return render_template('account_management/login.html', exist_message=existing_account)
+    return render_template('account_management/login.html')
 
 
     #   except Exception as e:
@@ -53,4 +53,4 @@ def index():
 #         return "Error creating user: " + str(e)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
