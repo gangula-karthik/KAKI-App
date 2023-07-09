5c44cbb608832affe02d670d00005492bf71a832
nightsinker <nightsinker.2005@gmail.com>
2023-07-09 15:25:21 +0800

    still tryna fix this eroror

--- Account_management/create_user.py
+++ Account_management/create_user.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
 
-cred = credentials.Certificate("Login\credentials.json")
+cred = credentials.Certificate("Account_management\credentials.json")
 
 firebase_admin.initialize_app(cred, {'databaseURL': "https://kaki-db097-default-rtdb.asia-southeast1.firebasedatabase.app/"})
 
