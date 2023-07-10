38ee589dd33ca6f53959b20155864ab4c5a8cd48
jun ming <mrngjunming@gmail.com>
2023-07-10 19:05:48 +0800

    updated the graph functions to have color (not yet tested)

--- Report_generation/Graph_functions.py
+++ Report_generation/Graph_functions.py
@@ -1,22 +1,29 @@
 import matplotlib.pyplot as plt
-import io
+from io import BytesIO
 import base64
 
 
-def plot_line_graph(month, quantity):
+def plot_line_graph(month, quantity,title):
 
 
     # Define the x and y values for the data points
     months = month
     quantities = quantity
 
+    fig, ax = plt.subplots()
+
     # Create the line graph
-    plt.plot(months, quantities, marker='o', linestyle='-', color='blue')
+    plt.plot(months, quantities, marker='o', linestyle='-', color='white')
+
+
+    ax.spines['bottom'].set_color('#17181C')  # Set color for the x-axis line
+    ax.spines['right'].set_color('#17181C')  # Make the y-axis line invisible
+
 
     # Add labels and title
-    plt.xlabel('Months')
-    plt.ylabel('Quantity')
-    plt.title('Monthly Quantity')
+    plt.xlabel('Months', color = 'white')
+    plt.ylabel('Quantity', color = 'white')
+    plt.title(title)
 
     # Convert the graph to a base64-encoded string
     buffer = io.BytesIO()
@@ -34,28 +41,33 @@ def plot_line_graph(month, quantity):
 
 
 
-def plot_pie_chart():
-    # Define the labels and sizes for the pie slices
-    labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4']
-    sizes = [25, 35, 20, 20]
+def create_pie_chart_with_legend():
+    # Sample data
+    sizes = [30, 20, 15, 10, 25]
+    labels = ['A', 'B', 'C', 'D', 'E']
+    colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF']
 
-    # Create the pie chart
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    # Create a figure and axis object
+    fig, ax = plt.subplots()
 
-    # Add a title
-    plt.title('Pie Chart')
+    # Plot the pie chart
+    wedges, _ = ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
 
-    # Convert the chart to a base64-encoded string
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
+    # Equal aspect ratio ensures that pie is drawn as a circle
+    ax.axis('equal')
+
+    # Create a legend at the side
+    ax.legend(wedges, labels, loc='center left', bbox_to_anchor=(1, 0.5))
+
+    # Save the plot to an in-memory buffer
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png', bbox_inches='tight')
     buffer.seek(0)
-    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-    buffer.close()
 
-    # Generate the HTML code for embedding the chart in a website
-    html_code = f'<img src="data:image/png;base64,{image_base64}" alt="Pie Chart">'
+    # Close the plot to free memory
+    plt.close()
 
-    # Return the HTML code
-    return html_code
+    # Return the image buffer
+    return buffer
 
 
