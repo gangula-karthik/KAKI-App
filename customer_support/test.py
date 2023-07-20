3b836874b39861b19520519d28a789d25e9118af
nightsinker <nightsinker.2005@gmail.com>
2023-07-20 21:28:27 +0800

    DESIGNER :PANDA

--- customer_support/test.py
+++ customer_support/test.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("customer_support/serviceAccountKey.json")
+cred = credentials.Certificate("Account_management\credentials.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://kaki-db097-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
