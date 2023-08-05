a7cb3474fad3f96d99c6c65fec192d0ec91dc66f
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-08-05 17:46:33 +0800

    add: FAQ generator flask

--- app.py
+++ app.py
@@ -24,8 +24,20 @@ from firebase_admin import db
 from flask_socketio import SocketIO, send
 from collections import OrderedDict
 from customer_support.comments import Comment
+from flask import Flask, jsonify, request
+from langchain import PromptTemplate, LLMChain
+import asyncio
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain, OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+import textwrap
+import json
 
-
+load_dotenv(find_dotenv())
+HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 
 config = {
@@ -115,10 +127,67 @@ def friendRequest():
     return render_template('friend_request.html', user_name=current_user)
 
 # Customer support routes
+
+template = """Question: {question}
+
+Using this information about helpdesk tickets: {formatted_template}
+
+Answer: Let's think step by step."""
+prompt_template = PromptTemplate(template=template, input_variables=["question", "formatted_template"])
+
+# See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+repo_id = "tiiuae/falcon-7b-instruct"
+falcon_llm = HuggingFaceHub(
+    repo_id=repo_id, model_kwargs={"temperature": 0.6, "max_new_tokens": 2000}, huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN
+)
+
+
+
+llm_chain = LLMChain(prompt=prompt_template, llm=falcon_llm)
+
+async def generate_faqs(tickets):
+    faqs = []
+
+    async def generate_faq(ticket):
+        subject = pyredb.child(f'tickets/{ticket}/subject').get().val()
+        description = pyredb.child(f'tickets/{ticket}/descriptions').get().val()
+        comments = pyredb.child(f'comments/').get().val()
+        comments_text = '\n'.join(comments) if comments else ''
+
+        formatted_template_data = f"Subject: {subject}\n\nDescription: {description}\n\nComments: {comments_text}"
+        questions = [
+            f"Could you elaborate on the issue reported in ticket {ticket}?",
+            f"What is the current status of ticket {ticket}?",
+            f"Who is currently handling ticket {ticket}?",
+            f"What are the next steps planned for resolving ticket {ticket}?"
+        ]
+
+        for question in questions:
+            formatted_prompt = {
+                "question": question,
+                "formatted_template": formatted_template_data
+            }
+            response = llm_chain.run(formatted_prompt)
+            faqs.append({
+                "question": question,
+                "answer": response
+            })
+
+    await asyncio.gather(*(generate_faq(ticket) for ticket in tickets))
+
+    return faqs
+
+
+
 @app.route('/support_overview', methods=['GET'])
 def customerOverview():
+
+    tickets = pyredb.child('tickets').get().val()  
+    faqs = asyncio.run(generate_faqs(tickets))
+
+
     messages = get_flashed_messages()
-    return render_template('customer_support/support_overview.html', user_name=current_user, messages=messages)
+    return render_template('customer_support/support_overview.html', user_name=current_user, messages=messages, faqs=faqs)
 
 @app.route('/user_chat', methods=['GET'])
 def staffChat():
