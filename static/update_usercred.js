b2b1e86f077e5e41f41ce7dac043f38759adfe27
nightsinker <nightsinker.2005@gmail.com>
2023-08-10 01:01:33 +0800

    add: update to css and js

--- static/update_usercred.js
+++ static/update_usercred.js
@@ -19,3 +19,28 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const deleteAccountBtn = document.getElementById("deleteAccountBtn");
+    
+    // Confirm before performing delete account action
+    deleteAccountBtn.addEventListener("click", function () {
+      const confirmDelete = confirm("Are you sure you want to delete your account?");
+      if (confirmDelete) {
+        // Perform the delete account action
+        // You can add your AJAX request or redirect logic here
+      }
+    });
+  
+    // Confirm before performing logout action
+    const logoutBtn = document.querySelector(".toggle-btn"); // Assuming the logout button has the "toggle-btn" class
+    logoutBtn.addEventListener("click", function () {
+      const confirmLogout = confirm("Are you sure you want to log out?");
+      if (!confirmLogout) {
+        event.preventDefault(); // Prevent the default action if the user cancels
+      }
+    });
+  
+    // No need for a confirmation alert for the "Update" button, as it's just submitting a form
+  });
